9f0187fd05268571f663a40995d35d4c58f05e25
jdev <juanj@jdev.com>
2026-05-07 15:37:18 -0600

    add Roles, Status Tracking

---
+++ src/main/java/org/delicias/common/dto/order/OrderTrackingStatus.java
@@ -0,0 +1,10 @@
+package org.delicias.common.dto.order;
+
+public enum OrderTrackingStatus {
+    ASSIGNED,
+    ROAD_TO_STORE,
+    AT_STORE,
+    ROAD_TO_DELIVERY,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/org/delicias/common/roles/Roles.java
@@ -0,0 +1,8 @@
+package org.delicias.common.roles;
+
+public class Roles {
+    public static final String MOBILE_USER_DELIVERY = "ROLE_MOBILE_USER_DELIVERY";
+    public static final String ROLE_MOBILE_USER = "ROLE_MOBILE_USER";
+
+    public static final String ROLE_WEB_RESTAURANT = "ROLE_WEB_RESTAURANT";
+}
