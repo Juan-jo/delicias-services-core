dd30beb34725584b5dae30d806bbffe3c3e702fa
jdev <juanj@jdev.com>
2026-05-14 09:12:13 -0600

    Add roles

--- src/main/java/org/delicias/common/roles/Roles.java
+++ src/main/java/org/delicias/common/roles/Roles.java
@@ -5,4 +5,5 @@ public class Roles {
     public static final String ROLE_MOBILE_USER = "ROLE_MOBILE_USER";
 
     public static final String ROLE_WEB_RESTAURANT = "ROLE_WEB_RESTAURANT";
+    public static final String ROLE_ROOT = "ROLE_ROOT";
 }
